3bd125bdf7a15a2f6aa34a9e69ffccf9f5ec5ba1
Alm0rs1k <91683934+Alm0rs1k@users.noreply.github.com>
2021-11-04 21:08:07 +0600

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -2,12 +2,21 @@ from pygame import *
 
 fps = 60
 game = True
+finish = False
 clock= time.Clock()
 win_height = 500
 win_width = 700
 window = display.set_mode((700, 500))
 display.set_caption('Ping Pong')
 background = transform.scale(image.load('phone.png'), (700, 500))
+
+speed_x = 3
+speed_y = 3
+
+font.init()
+font = font.Font(None, 50)
+lose_left = font.render('!PLAYER LEFT LOSE!', True, (180, 0, 0))
+lose_right = font.render('!PLAYER RIGHT LOSE!', True, (180, 0, 0))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -34,17 +43,39 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
+
 rocket_1 = Player("racket.png", 30, 10, 10, 100, 10)
 rocket_2 = Player("racket.png", 650, 10, 10, 100, 10)
+ball = GameSprite("tenis_ball.png", 200, 200, 50, 50, 200)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.blit(background, (0, 0))
-    rocket_1.update_l()
-    rocket_1.reset()
-    rocket_2.update_r()
-    rocket_2.reset()
-    display.update()
+    if finish != True:
+        window.blit(background, (0, 0))
+        rocket_1.update_l()
+        rocket_2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(rocket_1, ball) or sprite.collide_rect(rocket_2, ball):
+            speed_x *= -1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            time.delay(3000)
+            window.blit(lose_left, (200, 200))
+            game_over = False
+        if ball.rect.x > win_width:
+            finish = True
+            time.delay(3000)
+            window.blit(lose_right, (200, 200))
+            game = False
+
+        rocket_1.reset()
+        rocket_2.reset()
+        ball.reset()
     clock.tick(fps)
+    display.update()
